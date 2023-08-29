f7a2e12aa67808db89a96e2bd668a054aabcd8e1
Gabe R <9311953+Marsfan@users.noreply.github.com>
2023-08-28 20:31:42 -0700

    add unit tests for argument parser

--- src/arguments.rs
+++ src/arguments.rs
@@ -7,24 +7,28 @@
 use std::env;
 
 /// Structure holding the parsed arguments.
+#[derive(Debug, Eq, PartialEq)]
 pub struct Arguments {
     /// Path of the image to process.
     pub input_path: String,
 }
 
 impl Arguments {
-    /// Parse the arguments from the command line.
+    /// Parse the arguments from an iterator.
     ///
-    /// # Returns:
-    ///     A result that containing either the parsed arguments, or an error
-    ///     message.
-    pub fn from_cli() -> Result<Arguments, &'static str> {
-        let mut args = env::args();
+    /// # Arguments
+    /// * `args`: Iterator of arguments to parse.
+    ///
+    /// # Returns
+    /// The parsed arguments, or an error messsage if parsing failed.
+    fn parse<T>(mut args: T) -> Result<Self, &'static str>
+    where
+        T: Iterator<Item = String>,
+    {
         // Skip over the executable path
         args.next();
 
-        // Parse the input path argument.
-        let input_path = match args.next() {
+        let input_path: String = match args.next() {
             Some(arg) => arg,
             None => return Err("Path to image not supplied."),
         };
@@ -34,6 +38,51 @@ impl Arguments {
             return Err("To many arguments supplied.");
         }
 
-        return Ok(Arguments { input_path });
+        return Ok(Self { input_path });
+    }
+    /// Parse the arguments from the command line.
+    ///
+    /// # Returns
+    /// A result that containing either the parsed arguments, or an error
+    /// message.
+    pub fn from_cli() -> Result<Self, &'static str> {
+        return Self::parse(env::args());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Arguments;
+
+    #[test]
+    fn test_valid_args() {
+        let input_args = ["/bin/gcamera_tools", "motion_photo.jpg"]
+            .iter()
+            .map(|s| s.to_string());
+        let parsed_args = Arguments::parse(input_args);
+
+        let expected_result = Ok(Arguments {
+            input_path: String::from("motion_photo.jpg"),
+        });
+
+        assert_eq!(parsed_args, expected_result);
+    }
+
+    #[test]
+    fn test_no_enough_args() {
+        let input_args = ["/bin/gcamera_tools"].iter().map(|s| s.to_string());
+        let parsed_args = Arguments::parse(input_args);
+
+        assert_eq!(parsed_args, Err("Path to image not supplied."));
+    }
+
+    #[test]
+    fn test_too_many_args() {
+        let input_args = ["/bin/gcamera_tools", "motion_photo.jpg", "second_photo.jpg"]
+            .iter()
+            .map(|s| s.to_string());
+        let parsed_args = Arguments::parse(input_args);
+
+        assert_eq!(parsed_args, Err("To many arguments supplied."));
     }
 }
