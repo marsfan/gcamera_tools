4985ac9569cbcc8ac008de3bb7e30d22eddbcf6a
Gabe R <9311953+Marsfan@users.noreply.github.com>
2023-09-07 20:36:53 -0700

    Add tests for optional arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -36,9 +36,7 @@ pub struct Arguments {
 mod tests {
     use super::*;
 
-    // TODO: Document tests
-    // TODO: Tests for each optional argument
-
+    /// Test with none of the optional arguments
     #[test]
     fn test_valid_args_no_optionals() {
         let input_args = vec!["/bin/gcamera_tools", "motion_photo.jpg"];
@@ -53,6 +51,64 @@ mod tests {
         assert_eq!(parsed_args, expected_result);
     }
 
+    /// Test with image_output set.
+    #[test]
+    fn test_image_output() {
+        let input_args = vec![
+            "/bin/gcamera_tools",
+            "motion_photo.jpg",
+            "--image-output",
+            "just_photo.jpg",
+        ];
+        let parsed_args = Arguments::parse_from(input_args);
+        let expected_results = Arguments {
+            input_path: String::from("motion_photo.jpg"),
+            image_output: Some(String::from("just_photo.jpg")),
+            debug_output: None,
+            motion_output: None,
+        };
+        assert_eq!(parsed_args, expected_results);
+    }
+
+    /// Test with debug_output set
+    #[test]
+    fn test_debug_output() {
+        let input_args = vec![
+            "/bin/gcamera_tools",
+            "motion_photo.jpg",
+            "--debug-output",
+            "debug_data.bin",
+        ];
+        let parsed_args = Arguments::parse_from(input_args);
+        let expected_results = Arguments {
+            input_path: String::from("motion_photo.jpg"),
+            image_output: None,
+            debug_output: Some(String::from("debug_data.bin")),
+            motion_output: None,
+        };
+        assert_eq!(parsed_args, expected_results);
+    }
+
+    /// Test with video-output set
+    #[test]
+    fn test_video_output() {
+        let input_args = vec![
+            "/bin/gcamera_tools",
+            "motion_photo.jpg",
+            "--motion-output",
+            "motion.mp4",
+        ];
+        let parsed_args = Arguments::parse_from(input_args);
+        let expected_results = Arguments {
+            input_path: String::from("motion_photo.jpg"),
+            image_output: None,
+            debug_output: None,
+            motion_output: Some(String::from("motion.mp4")),
+        };
+        assert_eq!(parsed_args, expected_results);
+    }
+
+    /// Test for when not enough arguments are supplied
     // FIXME: Validate the error output somehow
     #[test]
     #[should_panic]
@@ -62,6 +118,7 @@ mod tests {
         parsed_args.unwrap();
     }
 
+    /// Test for when too many positional arguments are supplied
     // FIXME: Validate the error output somehow
     #[test]
     #[should_panic]
