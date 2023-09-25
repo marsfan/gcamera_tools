8f6140e7130f9acc213bc796914e1c1a8d55f4f1
Gabe R. <9311953+Marsfan@users.noreply.github.com>
2023-09-24 20:05:52 -0700

    Remove old fixme

--- src/camera_image.rs
+++ src/camera_image.rs
@@ -228,7 +228,6 @@ impl TryFrom<&[u8]> for CameraImage {
         if bytes[0..2] != vec![0xFF, 0xD8] {
             return Err(GCameraError::InvalidJpegMagic);
         }
-        // FIXME: Make try_from be for &[u8] so we don't need as_slice?
         let image = JpegImage::try_from(bytes)?;
 
         let (resources, resources_start) = match image.get_xmp() {
