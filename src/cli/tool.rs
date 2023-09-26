d94ed781380fcd5651ccb0d0cfce59222397c7fa
Gabe R. <9311953+Marsfan@users.noreply.github.com>
2023-09-26 00:58:35 -0700

    Function for creating default output path

--- src/cli/tool.rs
+++ src/cli/tool.rs
@@ -10,7 +10,29 @@
 use crate::camera_image::CameraImage;
 use crate::cli::arguments::Arguments;
 use clap::Parser;
+use std::path::{Path, PathBuf};
 use std::process::exit;
+
+/// Create the output file path from the provided argument, or a default value
+///
+/// If the given argument is `Some()`, use that value as the output path
+/// Otherwise, construct the output path by using the name of the input file
+/// with a custom extension.
+///
+/// # Arguments
+/// * `argument`: The argument used for a user to manually specify an output path
+/// * `input_path`: The input path of the image.
+/// * `default_extension`: The extension to use with the input path.
+fn create_output_path(
+    argument: Option<PathBuf>,
+    input_path: &Path,
+    default_extension: &str,
+) -> PathBuf {
+    return match argument {
+        Some(path) => path,
+        None => input_path.with_extension(default_extension),
+    };
+}
 
 /// Main function to be called when running the tool.
 pub fn tool_main() {
@@ -24,10 +46,7 @@ pub fn tool_main() {
 
     // Save the JPEG image if requested
     if args.save_image {
-        let output_path = match args.image_path {
-            Some(image_path) => image_path,
-            None => args.input_path.with_extension("image.jpg"),
-        };
+        let output_path = create_output_path(args.image_path, &args.input_path, "image.jpg");
         image.save_image(output_path).unwrap_or_else(|err| {
             eprintln!("Problem Saving JPEG Image: {err}");
             exit(1);
@@ -36,10 +55,7 @@ pub fn tool_main() {
 
     // Save the debug data if requested.
     if args.save_debug {
-        let output_path = match args.debug_path {
-            Some(debug_path) => debug_path,
-            None => args.input_path.with_extension("debug.bin"),
-        };
+        let output_path = create_output_path(args.debug_path, &args.input_path, "debug.bin");
         image.save_debug_data(output_path).unwrap_or_else(|err| {
             eprintln!("Problem Saving Debug Data: {err}");
             exit(1);
@@ -47,10 +63,7 @@ pub fn tool_main() {
     }
     // Save the motion photo if requested
     if args.save_motion {
-        let output_path = match args.motion_path {
-            Some(motion_path) => motion_path,
-            None => args.input_path.with_extension("motion.mp4"),
-        };
+        let output_path = create_output_path(args.motion_path, &args.input_path, "motion.mp4");
         image.save_motion_video(output_path).unwrap_or_else(|err| {
             eprintln!("Problem Saving Motion Video: {err}");
             exit(1);
